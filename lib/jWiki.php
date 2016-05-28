1fae66ef1f38e84d49ccc0fccd35548fd905c79e
Laurent Jouanneau <dev@ljouanneau.com>
2016-05-28 15:04:36 +0200

    jWiki should not rely on WIKIRENDERER_PATH

--- lib/jWiki.php
+++ lib/jWiki.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * @author     Laurent Jouanneau
-* @copyright  2006-2015 Laurent Jouanneau
+* @copyright  2006-2016 Laurent Jouanneau
 * @link       http://www.jelix.org
 * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -10,25 +10,28 @@
  * transform a wiki text into a document (html or else)
  * @link http://wikirenderer.jelix.org/
  */
-class jWiki extends  WikiRenderer {
+class jWiki extends WikiRenderer {
 
-    function __construct( $config=null){
+    function __construct( $config=null) {
 
-        if(is_string($config)){
-            $f = WIKIRENDERER_PATH.'rules/'.basename($config).'.php';
-            if(file_exists($f)){
-                require_once($f);
-                $this->config= new $config();
-            }else{
+        if (is_string($config)) {
+            if (class_exists($config)) {
+                // this is a class name 
+                $this->config = new $config();
+            }
+            else {
+                // this is a plugin name
                 $this->config = jApp::loadPlugin($config, 'wr_rules', '.rule.php', $config);
-                if (is_null($this->config))
+                if (is_null($this->config)) {
                     throw new Exception('Rules "'.$config.'" not found for jWiki');
+                }
             }
             $this->config->charset = jApp::config()->charset;
-        }elseif(is_object($config)){
-            $this->config=$config;
-        }else{
-            require_once(WIKIRENDERER_PATH . 'rules/wr3_to_xhtml.php');
+        }
+        elseif(is_object($config)) {
+            $this->config = $config;
+        }
+        else {
             $this->config= new wr3_to_xhtml();
             $this->config->charset = jApp::config()->charset;
         }
